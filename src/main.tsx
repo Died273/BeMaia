39573432b1191ef79fc987e7b9e346acf4424cc0
Diederick de Gier <diederick.de.gier@student.uva.nl>
2025-10-20 22:36:18 +0200

    New attempt fixing redirection

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,14 @@
+/* Restore deep link after GitHub Pages redirect */
+(function () {
+  try {
+    var pending = sessionStorage.getItem('redirectPath');
+    if (pending && typeof pending === 'string') {
+      sessionStorage.removeItem('redirectPath');
+      window.history.replaceState(null, '', pending);
+    }
+  } catch (_) {}
+})();
+
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";

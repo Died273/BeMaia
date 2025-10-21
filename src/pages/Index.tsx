a63767eadd42b67554cb9b8c5feb22de2b8700ef
Diederick de Gier <diederick.de.gier@student.uva.nl>
2025-10-21 12:06:05 +0200

    Changes howwework

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,7 +3,6 @@ import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import HowWeWork from "@/components/HowWeWork";
 import PartnerSection from "@/components/PartnerSection";
-import UniversityOrigin from "@/components/UniversityOrigin";
 import MarketOpportunity from "@/components/MarketOpportunity";
 import CTA from "@/components/CTA";
 import Footer from "@/components/Footer";
@@ -15,7 +14,6 @@ const Index = () => {
         <Hero />
         <MarketOpportunity />
         <HowWeWork />
-        <UniversityOrigin />
         <CTA />
         <PartnerSection />
       <Footer />
